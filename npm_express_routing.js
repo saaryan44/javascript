72e09110eebe54a32ec50c78d7b75b2331dacfce
rgb-PaperPatriot <57491566+saaryan44@users.noreply.github.com>
2021-10-14 22:08:19 +0530

    Update npm_express_routing.js
    express shorter syntax...node application.....routing.

--- npm_express_routing.js
+++ npm_express_routing.js
@@ -1,4 +1,6 @@
+/*npm init -y; or copy package.json with it; npm i express */
 const x=require("express")();
+
 x.get("/",(a,b)=>b.send(`<html><body><h1>hello routing</h1><img src="https://th.bing.com/th/id/R.821e3ff5d27bd8eb0c99a580386d29b3?rik=RJmXvLrLj%2f%2bPKQ&pid=ImgRaw&r=0" /></body></html>`));
 x.get("/r1",(a,b)=>b.send(`<html><body><img src="https://i.pinimg.com/originals/58/6e/51/586e51559dc40f48cd8ce65af9c1522f.gif" /></body></html>`));
 x.get("/r2",(a,b)=>b.send(`<html><body><img src="https://data.whicdn.com/images/287722949/original.gif" /></body></html>`));
